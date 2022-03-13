809760252b26a07566e466f02c9938360bac5baa
Marcelo Cárdenas <marcelo.cardenas.13@sansano.usm.cl>
2022-03-13 00:30:04 -0300

    Finished exercise G

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -10,6 +10,12 @@
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
@@ -18,6 +24,18 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) => { 
+                if x < 3.0 {
+                    2
+                }
+                else {
+                    1
+                }
+             },
+            Shot::Miss => 0
+        }
     }
 }
 
@@ -35,9 +53,29 @@ fn main() {
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
 
+    for coord in arrow_coords {
+        coord.print_description();
+        
+        let distance: f64 = coord.distance_from_center();
+        let shot: Shot;
 
+        if distance > 5.0 {
+            shot = Shot::Miss;
+        }
+        else if distance < 1.0 {
+            shot = Shot::Bullseye;
+        }
+        else {
+            shot = Shot::Hit(distance);
+        }
+
+        shots.push(shot);
+    }
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+    for shot in shots {
+        total += shot.points();
+    }
 
     println!("Final point total is: {}", total);
 }
